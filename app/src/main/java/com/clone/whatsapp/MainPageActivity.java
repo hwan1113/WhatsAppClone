3bdb342230c6edb36653eac34f5132f8c95c53d2
hwan1113 <hwan.privac@gmail.com>
2019-12-29 22:12:57 +0900

    initial commit

---
+++ app/src/main/java/com/clone/whatsapp/MainPageActivity.java
@@ -0,0 +1,36 @@
+package com.clone.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        //13. Get the Logout Component.
+        Button mLogout = findViewById(R.id.logout);
+        //14. Make the eventlistener of the button
+        mLogout.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                return;
+
+            }
+        });
+    }
+}
