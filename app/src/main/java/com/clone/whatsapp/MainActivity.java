89eff6f07c6de1f1f347d82a4b18ad9be0cda723
hwan1113 <hwan.privac@gmail.com>
2019-12-29 22:15:50 +0900

    Authentication added

---
+++ app/src/main/java/com/clone/whatsapp/MainActivity.java
@@ -0,0 +1,127 @@
+package com.clone.whatsapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    //  ３．Declare Variables
+    private EditText mPhoneNumber, mCode;
+    private Button mSend;
+    //7. Declare callback function based on Firebase Class
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
+    //16 Declare a verification field.
+    String mVerificationId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+
+        //11. Add this line. Whenever user is logged in, We skip the line below this logic.
+        userIsLoggedIn();
+
+        //４. Initiate Variables
+        mPhoneNumber = findViewById(R.id.phoneNumber);
+        mCode = findViewById(R.id.code);
+        mSend = findViewById(R.id.send);
+
+        // 5. Add event Listener on the verification code.
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MyApp","1");
+                if(mVerificationId != null) {
+                    verifyPhoneNumberWithCode();
+                } else {
+                    startPhoneNumberVerification();
+                }
+            }
+        });
+
+        //8.Define callback function here. When the verification happens or fails.
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                Log.d("MyApp","2");
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                Log.d("MyApp",e.toString());
+            }
+
+            //15. Firebase does not always send the code. But when it sends, user must get it.
+            @Override
+            public void onCodeSent(String verificationId, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                Log.d("MyApp","4");
+                super.onCodeSent(verificationId, forceResendingToken);
+                mVerificationId = verificationId;
+                mSend.setText("Verify Code");
+            }
+        };
+    }
+
+    //17. Verify Phone number with the code method
+    private void verifyPhoneNumberWithCode() {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, mCode.getText().toString());
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    //9. Define function that will happen when the Phone auth succeeds.
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+        //10. We use FirebaseAuth and we write callbacks of this function as well.
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    userIsLoggedIn();
+                }
+            }
+        });
+    }
+
+    //10. We define what will happen when the user passes fireBase auth
+    private void userIsLoggedIn() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null) {
+            startActivity(new Intent(getApplicationContext(), MainPageActivity.class));
+            finish();
+            return;
+        }
+    }
+
+    //6. Make Method Using Firebase function.
+    private void startPhoneNumberVerification() {
+        Log.d("MyApp","1");
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                mPhoneNumber.getText().toString(),
+                60,
+                TimeUnit.SECONDS,
+                this,
+                mCallbacks
+        );
+    }
+}
